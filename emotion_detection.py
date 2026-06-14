86bcdeeb0c89fa5212bcfe79eb9e8e2754f01189
shambhavilau <shambhavilau4.com>
2026-06-14 07:26:04 -0400

    update formatting of response

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,7 +1,14 @@
 import requests
+import json
 def emotion_detector(text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers= {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     input_json = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json = input_json, headers=headers)
-    return response.text
+    formatted_response = json.loads(response.text)
+    emotions = formatted_response['emotionPredictions'][0]['emotion']
+
+    # find dominant emotion
+    dominant_emotion = max(emotions, key=emotions.get)
+    emotions["dominant_emotion"] = dominant_emotion
+    return emotions
